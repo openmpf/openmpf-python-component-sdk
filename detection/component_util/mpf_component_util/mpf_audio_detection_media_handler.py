0b5ad2758f52876cad7e3f231ce8053da103fa19
cdglasz <cdglasz@ymail.com>
2019-03-27 14:16:15 -0400

    Add utility function for ripping audio from video files

--- detection/component_util/mpf_component_util/mpf_audio_detection_media_handler.py
+++ detection/component_util/mpf_component_util/mpf_audio_detection_media_handler.py
@@ -24,13 +24,61 @@
 # limitations under the License.                                            #
 #############################################################################
 
-from .image_reader import ImageReader, ImageReaderMixin
+import os
+import subprocess
 
-from .video_capture import VideoCapture, VideoCaptureMixin
+import mpf_component_api as mpf
 
-from .mpf_audio_detection_media_handler import rip_audio
+logger = mpf.configure_logging('mpf-audio-detection-media-handler.log', __name__ == '__main__')
 
-from .models_ini_parser import (ModelsIniParser, ModelNotFoundError, ModelsIniError, ModelFileNotFoundError,
-                                ModelEmptyPathError)
+def rip_audio(video_path, audio_path, start_time, stop_time):
+    logger.info((
+            'Ripping audio from {video_path:s} into {audio_path:s}'
+        ).format(
+            video_path=video_path,
+            audio_path=audio_path
+        )
+    )
 
-from .utils import *
+    # Resolve symbolic paths if necessary
+    video_path = os.path.realpath(video_path)
+    audio_path = os.path.realpath(audio_path)
+
+    # Confirm that input file exists
+    if not os.path.isfile(video_path):
+        raise ValueError("Input file does not exist: " + video_path)
+
+    # Confirm that output file can be created
+    audio_dir = os.path.split(audio_path)[0]
+    if not os.path.isdir(audio_dir):
+        raise ValueError("Output directory does not exist: " + audio_dir)
+
+    # Convert milliseconds to seconds
+    offset = start_time / 1000.0
+    duration = (stop_time - start_time) / 1000.0
+
+    # Construct ffmpeg call
+    command = (
+        'ffmpeg -i {video_path:s} -ss {offset:f} -t {duration:f} '
+        '-ac {channels:d} -ar {sampling_rate:d} -acodec {codec:s} '
+        '-af "highpass=f={highpass:d}, lowpass=f={lowpass:d}" -vn '
+        '-f {format:s} -y {audio_path:s} -loglevel error'
+    ).format(
+        video_path=video_path,
+        audio_path=audio_path,
+        offset=offset,
+        duration=duration,
+        channels=1,
+        sampling_rate=16000,
+        codec='pcm_s16le',
+        highpass=200,
+        lowpass=3000,
+        format='wav'
+    )
+
+    # Call ffmpeg
+    logger.info('query = %s', command)
+    subprocess.call(command, shell=True)
+
+    if not os.path.isfile(audio_path):
+        raise IOError("Unable to transcode input file: " + video_path)
