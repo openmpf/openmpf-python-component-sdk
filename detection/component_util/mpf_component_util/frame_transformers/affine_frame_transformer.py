Fix flip with rotation

detection/component_util/mpf_component_util/frame_transformers/affine_frame_transformer.py
@@ -25,22 +25,29 @@
 #############################################################################
 
 import functools
+from typing import Sequence, Iterable
 
 import cv2
 import numpy as np
 
-from .frame_transformer import BaseDecoratedFrameTransformer
+from .frame_transformer import BaseDecoratedFrameTransformer, IFrameTransformer
 from .. import utils
 from .search_region import SearchRegion
 
 
 class AffineFrameTransformer(BaseDecoratedFrameTransformer):
-    def __init__(self, regions, frame_rotation, frame_flip, search_region, inner_transform):
+    def __init__(self,
+                 regions: Sequence[utils.RotatedRect],
+                 frame_rotation: float,
+                 frame_flip: bool,
+                 search_region: SearchRegion,
+                 inner_transform: IFrameTransformer):
         super().__init__(inner_transform)
         self.__transform = _AffineTransformation(regions, frame_rotation, frame_flip, search_region)
 
     @staticmethod
-    def search_region_on_rotated_frame(rotation, flip, search_region, inner_transform):
+    def search_region_on_rotated_frame(rotation: float, flip: bool, search_region: SearchRegion,
+                                       inner_transform: IFrameTransformer) -> 'AffineFrameTransformer':
         return AffineFrameTransformer(
             _full_frame(inner_transform.get_frame_size(0)),
             rotation, flip, search_region, inner_transform)
@@ -68,11 +75,9 @@ class AffineFrameTransformer(BaseDecoratedFrameTransformer):
 
 
 class FeedForwardExactRegionAffineTransformer(BaseDecoratedFrameTransformer):
-    def __init__(self, regions, inner_transform):
+    def __init__(self, regions: Iterable[utils.RotatedRect], inner_transform: IFrameTransformer):
         super().__init__(inner_transform)
-        self.__frame_transforms \
-            = [_AffineTransformation(_single_region(*region), region[1], region[2], SearchRegion())
-               for region in regions]
+        self.__frame_transforms = [self.__create_transformation(r) for r in regions]
 
     def _do_frame_transform(self, frame, frame_index):
         return self.__get_transform(frame_index).apply(frame)
@@ -91,15 +96,18 @@ class FeedForwardExactRegionAffineTransformer(BaseDecoratedFrameTransformer):
                 'Attempted to get transformation for frame: {}, but there are only {} entries in the feed forward track'
                 .format(frame_index, len(self.__frame_transforms)))
 
+    @staticmethod
+    def __create_transformation(region: utils.RotatedRect):
+        frame_rotation = 360 - region.rotation if region.flip else region.rotation
+        return _AffineTransformation((region,), frame_rotation, region.flip, SearchRegion())
+
 
 
 def _single_region(region_rect, rotation, flip):
     return [(region_rect, rotation, flip)]
 
-def _full_frame(frame_size):
-    frame_rect = utils.Rect.from_corner_and_size((0, 0), frame_size)
-    return _single_region(frame_rect, 0, False)
-
+def _full_frame(frame_size: utils.Size) -> Sequence[utils.RotatedRect]:
+    return (utils.RotatedRect(0, 0, frame_size.width, frame_size.height, 0, False),)
 
 
 class _AffineTransformation(object):
@@ -109,7 +117,6 @@ class _AffineTransformation(object):
 
         self.__rotation_degrees = frame_rotation_degrees
         self.__flip = flip
-
 
         # Rotating an image around the origin will move some or all of the pixels out of the frame.
         rotation_mat = _IndividualXForms.rotation(360 - frame_rotation_degrees)
@@ -167,8 +174,6 @@ class _AffineTransformation(object):
 
     def apply_reverse(self, image_location):
         top_left = np.array((image_location.x_left_upper, image_location.y_left_upper, 1.0), dtype=float)
-        if self.__flip:
-            top_left[0] += image_location.width - 1
         new_top_left = np.matmul(self.__reverse_transformation_matrix, top_left)
 
         image_location.x_left_upper = int(round(new_top_left[0]))
@@ -176,7 +181,8 @@ class _AffineTransformation(object):
 
         if not utils.rotation_angles_equal(self.__rotation_degrees, 0):
             existing_rotation = utils.get_property(image_location.detection_properties, 'ROTATION', 0.0)
-            new_rotation = utils.normalize_angle(existing_rotation + self.__rotation_degrees)
+            rotation_adjustment_amount = 360 - self.__rotation_degrees if self.__flip else self.__rotation_degrees
+            new_rotation = utils.normalize_angle(existing_rotation + rotation_adjustment_amount)
             image_location.detection_properties['ROTATION'] = str(new_rotation)
 
         if self.__flip:
@@ -199,41 +205,14 @@ class _AffineTransformation(object):
         mapped_corners = np.matmul(simple_rotation, _AffineTransformation.__get_all_corners(regions))
         corner1 = np.amin(mapped_corners, axis=1)
         corner2 = np.amax(mapped_corners, axis=1)
-        size = corner2 - corner1 + 1
-        return utils.Rect.from_corner_and_size(corner1, size)
+        return utils.Rect.from_corners(corner1, corner2 + 1)
+
 
 
     @staticmethod
-    def __get_all_corners(regions):
+    def __get_all_corners(regions: Iterable[utils.RotatedRect]):
         # Matrix containing each region's 4 corners. First row is x coordinate and second row is y coordinate.
-        return np.hstack([_AffineTransformation.__get_corners(region, rot) for region, rot, _ in regions])
-
-    @staticmethod
-    def __get_corners(region, rotation):
-        radians = np.deg2rad(rotation)
-        sin_val = np.sin(radians)
-        cos_val = np.cos(radians)
-
-        x = region.x
-        y = region.y
-        # Need to subtract one because if you have a Rect(x=0, y=0, width=10, height=10),
-        # the bottom right corner is (9, 9) not (10, 10)
-        width = region.width - 1
-        height = region.height - 1
-
-        corner_2_x = x + width * cos_val
-        corner_2_y = y - width * sin_val
-
-        corner_3_x = corner_2_x + height * sin_val
-        corner_3_y = corner_2_y + height * cos_val
-
-        corner_4_x = x + height * sin_val
-        corner_4_y = y + height * cos_val
-
-        return (
-            (x, corner_2_x, corner_3_x, corner_4_x),
-            (y, corner_2_y, corner_3_y, corner_4_y)
-        )
+        return np.transpose(np.vstack([r.corners for r in regions]))
 
 
 

detection/component_util/mpf_component_util/utils.py
@@ -24,11 +24,13 @@
 # limitations under the License.                                            #
 #############################################################################
 
+import dataclasses
 import operator
 import sys
 import typing
-from typing import Any, Callable, Iterator, Mapping, NamedTuple, Optional, Sequence, Tuple, Union, TypeVar
+from typing import Callable, Mapping, NamedTuple, Optional, Sequence, Tuple, Union, TypeVar
 
+import cv2
 import numpy as np
 
 import mpf_component_api as mpf
@@ -59,20 +61,13 @@ def get_property(
         return default_value
 
 
-TKey = TypeVar('TKey')
-TVal = TypeVar('TVal')
-
-def dict_items_ordered_by_key(
-        dict_: Mapping[TKey, TVal],
-        key: Optional[Callable[[TKey], Any]] = None,
-        reverse: bool = False) -> Iterator[Tuple[TKey, TVal]]:
+def dict_items_ordered_by_key(dict_, key=None, reverse=False):
     ordered_keys = sorted(dict_, key=key, reverse=reverse)
     return ((k, dict_[k]) for k in ordered_keys)
 
 
-def dict_values_ordered_by_key(dict_: Mapping[TKey, TVal]) -> Iterator[TVal]:
+def dict_values_ordered_by_key(dict_):
     return (v for k, v in dict_items_ordered_by_key(dict_))
-
 
 
 def normalize_angle(angle: float) -> float:
@@ -89,7 +84,13 @@ def rotation_angles_equal(a1: float, a2: float, epsilon=0.1) -> bool:
 
 IntOrFloat = Union[int, float]
 
+
 class Point(NamedTuple):
+    """
+    The C++ OpenCV has a cv::Point class, but the Python version uses 2-tuples to represent points.
+    Since NamedTuple is a subclass of tuple, this class can be used as a parameter in OpenCV functions
+    that expect a point.
+    """
     x: IntOrFloat
     y: IntOrFloat
 
@@ -98,6 +99,11 @@ _PointLike = Union[Point, Tuple[IntOrFloat, IntOrFloat], Sequence[IntOrFloat]]
 
 
 class Size(NamedTuple):
+    """
+    The C++ OpenCV has a cv::Size class, but the Python version uses 2-tuples to represent sizes.
+    Since NamedTuple is a subclass of tuple, this class can be used as a parameter in OpenCV functions
+    that expect a size.
+    """
     width: IntOrFloat
     height: IntOrFloat
 
@@ -125,6 +131,11 @@ def element_wise_op(op, obj1, obj2, target_type=None):
 
 
 class Rect(NamedTuple):
+    """
+    The C++ OpenCV has a cv::Rect class, but the Python version uses 4-tuples to represent rectangles.
+    Since NamedTuple is a subclass of tuple, this class can be used as a parameter in OpenCV functions
+    that expect a rectangle.
+    """
     x: IntOrFloat
     y: IntOrFloat
     width: IntOrFloat
@@ -213,3 +224,81 @@ _RectLike = Union[
     Tuple[_PointLike, Size],
     Tuple[_PointLike, Point]
 ]
+
+
+@dataclasses.dataclass
+class RotatedRect:
+    """
+    Represents a rectangle that may or may not be axis aligned.
+    The algorithm to "draw" the rectangle, is as follows:
+    1. Draw the rectangle ignoring rotation and flip.
+    2. Stick a pin in the top left corner of the rectangle because the top left doesn't move,
+       but the rest of the rectangle may be moving.
+    3. Rotate the rectangle counter-clockwise the given number of degrees around its top left corner.
+    4. If the rectangle is flipped, flip horizontally around the top left corner.
+    """
+    x: float
+    y: float
+    width: float
+    height: float
+    rotation: float
+    flip: bool
+
+    @property
+    def corners(self) -> Tuple[Point, Point, Point, Point]:
+        has_rotation = not rotation_angles_equal(self.rotation, 0)
+        if not has_rotation and not self.flip:
+            return (
+                Point(self.x, self.y),
+                Point(self.x + self.width - 1, self.y),
+                Point(self.x + self.width - 1, self.y + self.height - 1),
+                Point(self.x, self.y + self.height - 1),
+            )
+
+        xform_mat = self._get_transform_mat()
+
+        tr_x = self.x + self.width - 1
+        br_y = self.y + self.height - 1
+        corner_mat = (
+            (tr_x, tr_x, self.x),
+            (self.y, br_y, br_y),
+            (1, 1, 1)
+        )
+
+        xformed_corners = np.matmul(xform_mat, corner_mat)
+        return (
+            Point(self.x, self.y),
+            Point(xformed_corners[0, 0], xformed_corners[1, 0]),
+            Point(xformed_corners[0, 1], xformed_corners[1, 1]),
+            Point(xformed_corners[0, 2], xformed_corners[1, 2])
+        )
+
+    def _get_transform_mat(self):
+        has_rotation = not rotation_angles_equal(self.rotation, 0)
+        assert has_rotation or self.flip
+
+        if has_rotation and not self.flip:
+            # This case is handled separately because it is the most common case and we can avoid some work.
+            return cv2.getRotationMatrix2D((self.x, self.y), self.rotation, 1)
+
+        # At this point, we know we need to flip since we already checked
+        # !hasRotation && !flip before calling this function.
+        flip_mat = (
+            (-1, 0, 2 * self.x),
+            (0, 1, 0)
+        )
+        if not has_rotation:
+            return flip_mat
+
+        rotation_mat2d = cv2.getRotationMatrix2D((self.x, self.y), self.rotation, 1)
+        # Add a row to the rotation matrix so it has the size required to multiply with the flip matrix.
+        full_rotation_mat = np.vstack((rotation_mat2d, (0, 0, 1)))
+
+        # Transform are applied from right to left, so rotation will occur before flipping.
+        return np.matmul(flip_mat, full_rotation_mat)
+
+
+    @property
+    def bounding_rect(self) -> Rect:
+        corners = np.asarray(self.corners)
+        return Rect.from_corners(np.min(corners, axis=0), np.max(corners, axis=0) + 1)